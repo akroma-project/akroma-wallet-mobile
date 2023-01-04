de6f85ce7fc0520f39eea875ce1548a2c2b8c36e
Jorge Alvarez <jorgealvarez@Jorges-MacBook-Pro.local>
2023-01-04 10:55:03 -0600

    feat: you can now list wallets and select them

--- src/screens/home/SendCoinScreen.tsx
+++ src/screens/home/SendCoinScreen.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
-import { ActivityIndicator, Alert, SafeAreaView, TouchableOpacity, View } from 'react-native';
+import { ActivityIndicator, Alert, SafeAreaView, ScrollView, TouchableOpacity, View, RefreshControl } from 'react-native';
 import GlobalStyles from '../../constants/GlobalStyles';
 import { useEffect, useState } from 'react';
 import { WalletContext } from '../../providers/WalletProvider';
-import { Input, Button, Text, Icon } from '@ui-kitten/components';
+import { Input, Button, Text, Icon, ListItem } from '@ui-kitten/components';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { HomeStackParamList } from '../../navigation/HomeStackNavigator';
 import { useNavigation } from '@react-navigation/core';
 import { Utils } from 'typesafe-web3/dist/lib/utils';
 import Toast from 'react-native-toast-message';
 import { TransactionCard } from '../../components/TransactionCard';
+import { WalletModel } from '../../data/entities/wallet';
+import _ from 'lodash';
 
 export const SendCoinScreen = ({ route }: { route: any }) => {
   type homeScreenProp = StackNavigationProp<HomeStackParamList, 'HomeScreen'>;
@@ -21,12 +23,33 @@ export const SendCoinScreen = ({ route }: { route: any }) => {
   const [status, setStatus] = useState('');
   const [showStatus, setShowStatus] = useState(false);
   const [sending, setSending] = React.useState(false);
-  const { send, state } = React.useContext(WalletContext);
+  const { send, state, loadWallets } = React.useContext(WalletContext);
   const u = new Utils();
 
   useEffect(() => {
     setAddress(sendToAddress);
   }, [sendToAddress]);
+
+  useEffect(() => {
+    async function init() {
+      setTimeout(async () => {
+        await loadWallets();
+      }, 500);
+    }
+    init();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onWalletPress = async (wallet: WalletModel) => {
+    setAddress(wallet.address);
+  };
+
+  const renderWalletRight = (item: WalletModel) => <Text>{item.lastBalance?.toString()}</Text>;
+
+  const [refreshing] = React.useState(false);
+  const onRefresh = async () => {
+    await loadWallets();
+  };
 
   const sendAmount = async () => {
     setSending(true);
@@ -122,12 +145,25 @@ export const SendCoinScreen = ({ route }: { route: any }) => {
         ) : (
           <View>
             <Input style={GlobalStyles.input} onChangeText={setAddress} value={address} placeholder="To" accessoryLeft={ContactIcon} accessoryRight={ScanIcon} />
-
             <Input style={GlobalStyles.input} onChangeText={setAmount} value={amount} placeholder="Amount to send" keyboardType="number-pad" />
             <Button disabled={invalid()} onPress={OnSendPress}>
               SEND
             </Button>
             <Text style={GlobalStyles.label}>A confirm dialog will open before sending</Text>
+            <ScrollView contentContainerStyle={GlobalStyles.scrollView} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+              {/* {state.wallets.length < 1 && <NoWallet />} */}
+              {state.wallets.length > 0 &&
+                _.sortBy(state.wallets, x => x.address).map((item, index) => (
+                  <ListItem
+                    key={index}
+                    title={item.name}
+                    description={item.address}
+                    onPress={() => onWalletPress(item)}
+                    // accessoryLeft={renderWalletLeft}
+                    // accessoryRight={() => renderWalletRight(item)}
+                  />
+                ))}
+            </ScrollView>
           </View>
         )}
       </View>
