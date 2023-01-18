47a2d2f580c3174b136c3186a634d96bb43b3e98
Luigy08 <luigy9696@gmail.com>
2023-01-18 11:13:07 -0600

    chore: watch wallet section

--- src/components/TopWallets.tsx
+++ src/components/TopWallets.tsx
@@ -7,33 +7,49 @@ import { WalletCard } from './WalletCard';
 interface Props {
   wallets: WalletModel[];
 }
+interface WalletsSectionParams {
+  title: string;
+  wallets: WalletModel[];
+  style?: any;
+}
+const WalletsSection = (params: WalletsSectionParams) => (
+  <View style={params.style}>
+    <Text style={styles.subTitle}>{params.title}</Text>
+    {params.wallets?.map(wallet => (
+      <TouchableHighlight underlayColor="#DDDDDD" key={wallet.id} onPress={() => console.log(wallet.address)}>
+        <View>
+          <WalletCard wallet={wallet} />
+          <Divider />
+        </View>
+      </TouchableHighlight>
+    ))}
+  </View>
+);
 export const TopWallets = ({ wallets }: Props) => {
   const [walletsState, setWalletsState] = useState<WalletModel[]>();
+  const [watchWallets, setWatchWallets] = useState<WalletModel[]>();
 
   useEffect(() => {
     const removeWatchedWallets = wallets.filter(element => element.encrypted !== 'watch');
     const orderWallets = removeWatchedWallets.sort((a, b) => Number(b.lastBalance) - Number(a.lastBalance));
     const topWallets = orderWallets.slice(0, 3);
     setWalletsState(topWallets);
+    const tempWatchwallet = wallets.filter(element => element.encrypted === 'watch');
+    setWatchWallets(tempWatchwallet);
   }, [wallets]);
   return (
     <View style={[GlobalStyles.walletsContainer]}>
       <Text style={styles.title}>Wallets</Text>
-      <Text style={styles.subTitle}>My Wallets</Text>
-      {walletsState &&
-        walletsState.map(wallet => (
-          <TouchableHighlight underlayColor="#DDDDDD" key={wallet.id} onPress={() => console.log(wallet.address)}>
-            <View>
-              <WalletCard wallet={wallet} />
-              <Divider />
-            </View>
-          </TouchableHighlight>
-        ))}
+      <WalletsSection title={'My Wallets'} wallets={walletsState} />
+      {watchWallets.length > 0 && <WalletsSection title={'Watched Wallets'} wallets={watchWallets} style={styles.walletsSection} />}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  walletsSection: {
+    paddingTop: 30,
+  },
   title: {
     fontSize: 18,
     paddingHorizontal: 20,
