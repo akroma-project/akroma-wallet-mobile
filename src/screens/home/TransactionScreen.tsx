124339985335eed8713921fad076e3a91b711589
Christopher A. Flores <christopher.flores@codexitos.com>
2023-01-25 11:36:00 -0600

    chore: solve linter errors

--- src/screens/home/TransactionScreen.tsx
+++ src/screens/home/TransactionScreen.tsx
@@ -1,14 +1,11 @@
-import React, { useRef } from 'react';
-import { useEffect } from 'react';
+import React from 'react';
 import { useState } from 'react';
-import { Animated } from 'react-native';
 import { HomeTransferButtons } from '../../components/HomeTransferButtons';
 import { TransactionSection } from '../../components/TransactionSection';
 import MainLayout from '../../layout/MainLayout';
 
 export const TransactionScreen = () => {
-  const [displayButtons, setDisplayButtons] = useState(undefined);
-  const fadeAnim = useRef(new Animated.Value(1)).current;
+  const [, setDisplayButtons] = useState(undefined);
 
   return (
     <MainLayout>
