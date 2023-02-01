d742c6733956b857c3487aef9205a76e61659a1a
Christopher A. Flores <christopher.flores@codexitos.com>
2023-02-01 08:17:46 -0600

    chore: solve lint errors

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { SafeAreaView } from 'react-native';
 import GlobalStyles from '../constants/GlobalStyles';
 
