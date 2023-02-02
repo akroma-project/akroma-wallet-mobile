9d6e1e9a223e3b4a1a38c4bc5ebf4668fb906b30
Jesus Vasquez <adonay16.vz@gmail.com>
2023-02-01 18:18:14 -0600

    feat: modify input to use QR scan

--- src/screens/home/ImportWalletWatch.tsx
+++ src/screens/home/ImportWalletWatch.tsx
@@ -1,23 +1,31 @@
 import * as React from 'react';
-import { ActivityIndicator, Keyboard, SafeAreaView, TouchableWithoutFeedback, View } from 'react-native';
+import { ActivityIndicator, Keyboard, SafeAreaView, TouchableWithoutFeedback, View, TouchableOpacity } from 'react-native';
 import GlobalStyles from '../../constants/GlobalStyles';
 import { useState, useContext, useEffect } from 'react';
-import { Button, Input } from '@ui-kitten/components';
+import { Button, Icon, Input, Text } from '@ui-kitten/components';
 import { useDatabaseConnection } from '../../data/connection';
 import { ImageOverlay } from '../../extra/image-overlay.component';
 import { isAddress } from 'ethers/lib/utils';
 import Toast from 'react-native-toast-message';
 import { WalletContext } from '../../providers/WalletProvider';
 import { GlobalContext } from '../../providers/GlobalProvider';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/core';
+import { HomeStackParamList } from '../../navigation/HomeStackNavigator';
 
 export const ImportWalletWatch = () => {
   const { walletsRepository } = useDatabaseConnection();
   const [walletAddress, walletAddressChange] = useState('');
   const [name, setName] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
+  const [address, setAddress] = useState('');
+
   const isValidAddress = isAddress(walletAddress);
   const { addWallet } = useContext(WalletContext);
   const { newWatchWallet, setNewWatchWallet } = useContext(GlobalContext);
+
+  type homeScreenProp = StackNavigationProp<HomeStackParamList, 'HomeScreen'>;
+  const navigator = useNavigation<homeScreenProp>();
 
   useEffect(() => {
     walletAddressChange(newWatchWallet);
@@ -58,6 +66,14 @@ export const ImportWalletWatch = () => {
     }, 1500);
   };
 
+  const ScanIcon = (props: any) => {
+    return (
+      <TouchableOpacity onPress={() => navigator.navigate('ScannerScreen')}>
+        <Icon {...props} name="video" />
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
       <SafeAreaView style={GlobalStyles.flex}>
@@ -66,9 +82,11 @@ export const ImportWalletWatch = () => {
             <ActivityIndicator size="large" />
           ) : (
             <View style={GlobalStyles.container}>
-              <View>
+              <View style={{ marginTop: 30 }}>
+                <Text style={{ color: 'white', fontSize: 14, paddingBottom: 8 }}>Address</Text>
+                <Input style={GlobalStyles.input} onChangeText={setAddress} value={address} placeholder="To" accessoryRight={ScanIcon} />
                 <Input style={GlobalStyles.input} onChangeText={setName} value={name} placeholder="Wallet name, min 5 chars" disabled={loading} />
-                <Input style={GlobalStyles.input} onChangeText={walletAddressChange} value={walletAddress} placeholder="Wallet Address" disabled={loading} />
+                <Input style={GlobalStyles.input} onChangeText={walletAddressChange} value={walletAddress} placeholder="Enter address or Scan QR code" disabled={loading} />
                 <Button disabled={loading} onPress={async () => await OnImportPress()}>
                   IMPORT
                 </Button>
