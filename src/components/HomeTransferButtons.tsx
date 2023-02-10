b0eb9cdae147ab8e75b502d5bf2ac910a890f3de
Jesus Vasquez <adonay16.vz@gmail.com>
2023-02-10 15:06:39 -0600

    feat: Buttons send & receive working in wallet detail

--- src/components/HomeTransferButtons.tsx
+++ src/components/HomeTransferButtons.tsx
@@ -1,16 +1,23 @@
 import { Button, Text } from '@ui-kitten/components';
 import React from 'react';
 import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { HomeStackParamList } from '../navigation/HomeStackNavigator';
+import { StackNavigationProp } from '@react-navigation/stack';
 import ArrowdownSvg from '../assets/svg/ArrowdownSvg';
 import ArrowupSvg from '../assets/svg/ArrowupSvg';
 import GlobalStyles from '../constants/GlobalStyles';
+
 export const HomeTransferButtons = () => {
+  type homeScreenProp = StackNavigationProp<HomeStackParamList, 'HomeScreen'>;
+  const navigator = useNavigation<homeScreenProp>();
+
   return (
     <View style={[GlobalStyles.transferButtonsContainer]}>
-      <Button accessoryLeft={() => <ArrowupSvg />} style={GlobalStyles.transferButton} status="control">
+      <Button accessoryLeft={() => <ArrowupSvg />} style={GlobalStyles.transferButton} onPress={() => navigator.navigate('SendCoinScreen')} status="control">
         {() => <Text style={GlobalStyles.textButton}>Send</Text>}
       </Button>
-      <Button accessoryLeft={() => <ArrowdownSvg />} style={GlobalStyles.transferButton} status="control">
+      <Button accessoryLeft={() => <ArrowdownSvg />} style={GlobalStyles.transferButton} onPress={() => navigator.navigate('ReceiveCoinScreen')} status="control">
         {() => <Text style={GlobalStyles.textButton}>Receive</Text>}
       </Button>
     </View>
